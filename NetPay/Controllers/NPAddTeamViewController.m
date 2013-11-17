f69408f611c42ca118fe94e5fef01ff4a3843dd6
jurre <jurrestender@hotmail.com>
2013-11-16 16:47:55 -0800

    basic logo

---
+++ NetPay/Controllers/NPAddTeamViewController.m
@@ -0,0 +1,38 @@
+//
+//  NPAddTeamViewController.m
+//  NetPay
+//
+//  Created by Jurre Stender on 16/11/13.
+//  Copyright (c) 2013 NetPay. All rights reserved.
+//
+
+#import "NPAddTeamViewController.h"
+
+@interface NPAddTeamViewController ()
+
+@end
+
+@implementation NPAddTeamViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
